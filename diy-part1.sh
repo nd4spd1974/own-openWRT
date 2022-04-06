e9c3dfc4454aeaaf6f39e5ac1e76e6940091137d
nd4spd1974 <81602210+nd4spd1974@users.noreply.github.com>
2022-04-06 19:59:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,5 @@
 
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git bypass https://github.com/kiddin9/openwrt-bypass' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#echo 'src-git bypass https://github.com/kiddin9/openwrt-bypass' >>feeds.conf.default
